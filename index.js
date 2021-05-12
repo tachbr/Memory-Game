4040aa5406596b916c319ca9c15cad727024f35e
tachbr <70528509+tachbr@users.noreply.github.com>
2021-05-12 23:51:13 +0200

    add images

---
+++ index.js
@@ -0,0 +1,44 @@
+const sushiImg = '/images/sushi_';
+
+
+
+
+function createCardDeck() {
+
+}
+
+function shuffleDeck(numCards) {
+
+  for (let i = 0; i < numCards/2; i++) {
+
+    deckArray[randomIndex] = sushiImg + i + '.png';
+
+
+  }
+// generate random number between 0 and numCards
+// place in Array
+// repeat
+// if same number repeat
+
+}
+
+
+function dealDeck(x) {
+  // lay out the cards in formation x rows by x columns
+  for ( let card in deckArray) {
+    
+  }
+}
+
+function turnCardUp() {
+
+}
+
+function turnCardDown() {
+
+}
+
+
+function compareCards() {
+
+}
